2d1f8a3aef7fead87e00e6d6b3c8fa8b1a04d982
deynekosergey <deynekosergey38@gmail.com>
2024-12-06 19:29:55 +0300

    3rd_hw.3

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,11 +1,10 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
-let number1 = 10
-let number2 = 7
 
 function isEven(number1, number2) {
-   return number1 % 2 === 0 || number2 % 2 !== 0
+    if (number1 % 2 === 0 || number2 % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
-console.log(isEven(number1))
-console.log(isEven(number2))
